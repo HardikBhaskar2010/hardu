03b39b162c9392789422178c6247212d374d381a
HardikBhaskar2010 <119751315+HardikBhaskar2010@users.noreply.github.com>
2023-04-09 12:17:30 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,64 @@
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Enter your name");
+    this.playButton = createButton("Play");
+    this.titleImg = createImg("./assets/title.png", "game title");
+    this.greeting = createElement("h2");
+    this.resetButton = createButton("Reset");
+  }
+
+  setElementsPosition() {
+    this.titleImg.position(120, 50);
+    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
+    this.resetButton.position(width-200, height-570)
+  }
+
+  setElementsStyle() {
+    this.titleImg.class("gameTitle");
+    this.input.class("customInput");
+    this.playButton.class("customButton");
+    this.greeting.class("greeting");
+    this.resetButton.class("customButton")
+  }
+
+  //BP
+  hide() {
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+  }
+
+  //BP
+  handleMousePressed() {
+    this.playButton.mousePressed(() => {
+      this.input.hide();
+      this.playButton.hide();
+      var message = `
+      Hello ${this.input.value()}
+      </br>wait for another player to join...`;
+      this.greeting.html(message);
+      playerCount += 1;
+      player.name = this.input.value();
+      player.index = playerCount;
+    player.addPlayer();//aa
+      player.updateCount(playerCount); // BP
+     player.getDistance(); //aa
+    });
+    this.resetButton.mousePressed(() => {
+      database.ref('/').set({
+        playerCount:0,
+        gameState:0,
+        players: {}
+      });
+      window.location.reload();
+     });
+  }
+
+  display() {
+    this.setElementsPosition();
+    this.setElementsStyle();
+    this.handleMousePressed();
+  }
+}
